9bd0de7c0d8d68fdee7ca8b935b4c53ff4ec4e8a
ChaibiP <pierrechaibi@gmail.com>
2026-07-17 11:28:43 +0200

    Ajout de streamlit , Visualisation sur page web des 3 graphiques ainsi que les données essentielles
    Prochaine avancée : essayer de créer d'autres graphiques et/ou rendre plus pertinente les données actuelles

---
+++ app.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.set_page_config(
+    page_title="OM Data Analytics",
+    layout="wide"
+)
+
+players = pd.read_csv(
+    "data/players.csv",
+    sep=";"
+)
+
+st.title("OM Data Analytics Dashboard")
+st.write(
+    "Analyse des performances des joueurs ayant joué plus de 500 minutes."
+)
+total_players = len(players)
+total_goals = players["Goals"].sum()
+total_assists = players["assists"].sum()
+average_rating = players["rating"].mean()
+col1, col2, col3, col4 = st.columns(4)
+
+col1.metric(
+    "Joueurs analysés",
+    total_players
+)
+col2.metric(
+    "Buts",
+    total_goals
+)
+col3.metric(
+    "Passes décisives",
+    total_assists
+)
+col4.metric(
+    "Note moyenne",
+    round(average_rating,2)
+)
+top_scorers = players.sort_values(
+    "Goals",
+    ascending=False
+).head(5)
+top_scorers_graph = px.bar(
+    top_scorers,
+    x="Name",
+    y="Goals",
+    title="Top 5 buteurs"
+)
+st.plotly_chart(top_scorers_graph)
+position_count = players["Position"].value_counts()
+lineup_graph = px.pie(
+    position_count,
+    values=position_count.values,
+    names=position_count.index,
+    title="Composition de l'effectif ayant +500min"
+)
+st.plotly_chart(lineup_graph)
+rating_consistency_graph = px.scatter(
+    players,
+    x="Games",
+    y="rating",
+    size="Goals",
+    hover_name="Name",
+    title="Régularité et impact des joueurs"
+)
+st.plotly_chart(rating_consistency_graph)
