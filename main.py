14496d5d096b319947154ff899ed7eae3fddc843
ChaibiP <pierrechaibi@gmail.com>
2026-07-17 11:06:29 +0200

    Changement du csv, data cleaning + affichage du premier graphique
    Data cleaning afin d'avoir 4 differentes positions : Attaquant , Milieu , Defenseur , Gardien afin de pouvoir les afficher plus facilement et mieux s'y retrouver. Premiers graphiques meilleurs buteurs et Positions de l'effectif. Mise en place d'un top 3 joueurs par poste selon les notes globales.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import plotly.express as px
 
 players = pd.read_csv("data/players.csv", sep=";")
 
@@ -6,7 +7,77 @@ top_scorers = players.sort_values(
     "Goals",
     ascending=False
 )
+
+best_players_by_rating = players.sort_values(
+    "rating",
+    ascending=False
+)
+top_assisters=players.sort_values(
+    "assists",
+    ascending=False
+)
+
+print("Tout les joueurs de l'effecitf : ")
 print(players)
-print(players.head())
+print("5 joueurs avec la meilleure note generale de l'effectif : ")
+print(best_players_by_rating.head(5))
+print("5 meilleurs buteurs de l'effectif : ")
 print(top_scorers.head(5))
-players.info()
+print("5 meilleurs passeurs de l'effectif : ")
+print(top_assisters.head(5))
+position_count = players["Position"].value_counts()
+print(position_count)
+total_players = len(players)
+total_goals = players["Goals"].sum()
+total_assists = players["assists"].sum()
+average_rating = players["rating"].mean()
+stats = {
+    "Joueurs analysés": total_players,
+    "Buts totaux": total_goals,
+    "Passes décisives": total_assists,
+    "Note moyenne": round(average_rating, 2)
+}
+print(stats)
+attackers = players[
+    players["Position"] == "Attaquant"
+]
+
+best_attackers=attackers.sort_values(
+        "rating",
+        ascending=False
+    )
+best_attackers=best_attackers.head(3)
+defenders=players[
+    players["Position"] == "Defenseur"
+]
+best_defenders=defenders.sort_values(
+    "rating",
+    ascending=False
+)
+best_defenders=best_defenders.head(3)
+
+midfielders=players[
+    players["Position"]=="Milieu"
+]
+best_midfielders=midfielders.sort_values(
+    "rating",
+    ascending=False
+)
+best_midfielders=best_midfielders.head(3)
+
+
+fig = px.bar(
+    top_scorers,
+    x="Name",
+    y="Goals",
+    title="Top 5 buteurs - OM (+500min)"
+)
+fig.show()
+fig = px.pie(
+    position_count,
+    values=position_count.values,
+    names=position_count.index,
+    title="Composition de l'effectif ayant +500min"
+)
+
+fig.show()
